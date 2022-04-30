96218a0dfaa76c34327036cd501ab9293243a46c
JeykerSalinas <jeyker.salinas13@gmail.com>
2022-04-29 23:14:03 -0400

    My first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,143 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <div class="shadow">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div class="container-fluid justify-content-between">
+          <a class="navbar-brand" href="#">Caudio Rodríguez</a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div
+            class="collapse navbar-collapse d-flex justify-content-end"
+            id="navbarNavDropdown"
+          >
+            <ul class="navbar-nav px-4">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#galery">Galerías</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Colecciones</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Sobre Claudio</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdownMenuLink"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Reseñas
+                </a>
+                <ul
+                  class="dropdown-menu"
+                  aria-labelledby="navbarDropdownMenuLink"
+                >
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                  </li>
+                </ul>
+              </li>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdownMenuLink"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Catálogos y Libros
+                </a>
+                <ul
+                  class="dropdown-menu"
+                  aria-labelledby="navbarDropdownMenuLink"
+                >
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
     <router-view />
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      Contacto (1 subpágina: correo electrónico e instagram)
+    </nav>
   </div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  margin-top: 56px;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,69 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div id="presentation">
+      <p>Presentación (1 subpágina) Hero section</p>
+    </div>
+    <div
+      id="galery"
+      class="d-md-flex justify-content-around p-5 align-items-center"
+    >
+      <div class="card bg-dark text-white w-25 h-25 mx-auto">
+        <img
+          src="https://4.bp.blogspot.com/-IhAGbTZ3osA/UXfo4IVkPaI/AAAAAAABFvU/-gq2rD-C1MY/s1600/pinturas-famosas.jpg"
+          class="card-img"
+          alt="..."
+        />
+        <div class="card-img-overlay">
+          <h5 class="card-title">Galeria 1</h5>
+        </div>
+      </div>
+      <div class="card bg-dark text-white w-25 h-25 mx-auto">
+        <img
+          src="https://mymodernmet.com/wp/wp-content/uploads/2020/07/denis-shiryaev-neural-network-art-2.jpg"
+          class="card-img"
+          alt="..."
+        />
+        <div class="card-img-overlay">
+          <h5 class="card-title">Galeria 2</h5>
+        </div>
+      </div>
+      <div class="card bg-dark text-white w-25 h-25 mx-auto">
+        <img
+          src="https://i0.wp.com/www.freim.tv/wp-content/uploads/2017/03/la-persistencia-de-la-memoria-salvador-dali-oleo-pintura-D_NQ_NP_954917-MLM27168877544_042018-F.jpg"
+          class="card-img"
+          alt="..."
+        />
+        <div class="card-img-overlay">
+          <h5 class="card-title">Galeria 3</h5>
+        </div>
+      </div>
+    </div>
+    <p>Algunas colecciones que lo albergan (1 subpágina)</p>
+    <p>Claudio, calidad expresiva (1 subpágina)</p>
+    <p>Reseñas en periódicos (18 subpáginas que a su vez..)</p>
+    <p>
+      Catálogos y libros (19 carpetas, que a su vez cada cada una contiene
+      varias subpáginas.)
+    </p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
-  },
 };
 </script>
+<style scoped>
+#presentation {
+  background: url("https://simef.cl/imgs_1-966/storage_Arte-moderno-abstracto-famosas-pinturas-de-arte-moderno.jpeg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 95vh; /*borrar despues*/
+}
+#galery {
+  background-color: #212121;
+}
+</style>
