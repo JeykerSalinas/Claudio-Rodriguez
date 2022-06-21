3be627f4ded483d7c06a12ef965010aaa69c326e
Jeyker <jeyker.salinas13@gmail.com>
2022-06-20 23:03:10 -0400

    fix

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import VueAgile from "vue-agile";
-import "@/assets/style/main.scss";
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
